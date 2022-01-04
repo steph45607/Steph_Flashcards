91bdbf2261fcfcde39f215cb7660f3c1cd75a0fe
steph45607 <staniswinata.stephanie@gmail.com>
2022-01-04 15:48:51 +0700

    create dictionary card library

--- example/create.py
+++ example/create.py
@@ -1,19 +1,23 @@
-with open("example/english.txt", "r") as f:
+file = "/Users/steph/Desktop/Biodata.txt"
+with open(file, "r") as f:
 
     # To split lines and assign to a list
-    # With <word> \t <word>
+    # With <word>\<word>
     match = []
     for i in f:
         match.append(i)
-    print(match)
+    # Remove \n from the end of the line
+    match = map(lambda s: s.strip(), match)
 
-    # To split every element inside the list by \t
-    # Assign it to a dictionary
-    words = []
-    for i in f:
-        for i in match:
-            i.split("\t")
-            words.append(i)
-    
-    print(words)
+    # To split every element inside the list by \
+    # Assign it to a list
+    words = [i.split("\\") for i in match]
+
+
+    # Change to dictionary
+    card = {}
+    for i in range(len(words)):
+        card[words[i][0]] = words[i][1]
+
+    print(card)
     
